c20d5112673ff4b7a72ca2c26d27c5e5c3a3fae7
Michelle Jordan <tmsmith1150@live.com>
2020-09-16 16:48:18 -0400

    worked with scripts

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes/api");
+
+//UNCOMMENT AFTER ROUTES  ARE CREATED
+// const routes = require("./routes/api");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,12 +15,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+
+//UNCOMMENT AFTER ROUTES ARE CREATED
+// app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
