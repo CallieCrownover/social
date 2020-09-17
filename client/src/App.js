a2f545e1bc35a6cefad7027c7a8b064885f1ace1
Michelle Jordan <tmsmith1150@live.com>
2020-09-17 16:43:04 -0400

    added components jumbotron and reactscroll

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Login from "./pages/Login";
-import Event from "./pages/Events"
+import Events from "./pages/Events";
+import MyEvents from "./pages/MyEvents";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Login/>
-      <Event/>
+    <Login />
     </div>
+    </Router>
   );
 }
 
--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -5,10 +5,12 @@ import "./style.css";
 // That way we don't have to define them all individually
 function LoginForm(props) {
   return (
-    <div className="form-group" class="animate__animated animate__zoomInUp animate__delay-2s">
+    <div className="form-div">
+    <div className="form-group" class="animate__animated animate__zoomInUp animate__delay-12s">
       <span>
       <input className="form-control"/>
       </span>
+    </div>
     </div>
   );
 }
