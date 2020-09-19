7e864fa881fec93fa4308722da92ce57b45e1a0c
jdsheridan02 <jdsheridan02@gmail.com>
2020-09-19 11:22:18 -0400

    added basic form info to lay foundation for adding event into database

---
+++ client/src/pages/CreateEvent.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react"; 
+
+function CreateEvent() {
+   
+    return (
+<form>
+<Input
+  onChange={handleInputChange}
+  name="title"
+  placeholder="Title (required)"
+/>
+<Input
+  onChange={handleInputChange}
+  name="author"
+  placeholder="Author (required)"
+/>
+<TextArea
+  onChange={handleInputChange}
+  name="synopsis"
+  placeholder="Synopsis (Optional)"
+/>
+<FormBtn
+  disabled={!(formObject.author && formObject.title)}
+  onClick={handleFormSubmit}
+>
+  Submit Book
+</FormBtn>
+</form>
+    )
+  }
+
+
+  export default CreateEvent;
+  
