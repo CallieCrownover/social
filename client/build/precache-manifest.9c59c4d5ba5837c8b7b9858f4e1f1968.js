dfa737869f4d79639f95bb1cd9b2791c0b9dd7e1
jdsheridan02 <jdsheridan02@gmail.com>
2020-10-01 19:46:06 -0400

    fixed api route to post data into event table

---
+++ client/build/precache-manifest.9c59c4d5ba5837c8b7b9858f4e1f1968.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aeba830e72c96b2bd50",
+    "url": "/static/css/main.16405888.chunk.css"
+  },
+  {
+    "revision": "7aeba830e72c96b2bd50",
+    "url": "/static/js/main.d6367cb6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6eb97514fd30967a7547",
+    "url": "/static/css/2.0e98e92e.chunk.css"
+  },
+  {
+    "revision": "6eb97514fd30967a7547",
+    "url": "/static/js/2.b0e3e545.chunk.js"
+  },
+  {
+    "revision": "71001f5b0721c3d0312bdf63cc9aa018",
+    "url": "/static/media/outdoor-events-stock-photo.71001f5b.jpg"
+  },
+  {
+    "revision": "0dfa7689dd4c1b51cc7816ca3b809168",
+    "url": "/static/media/art-events-stock-photo.0dfa7689.jpg"
+  },
+  {
+    "revision": "dc144f13753617335d7d48373122dac4",
+    "url": "/static/media/book-club-stock-photo.dc144f13.jpg"
+  },
+  {
+    "revision": "1e67c14ad8e4ff93e854740b8f784340",
+    "url": "/static/media/family-stock-photo.1e67c14a.jpg"
+  },
+  {
+    "revision": "a25b218049f3c29927bfd9e9dc525f92",
+    "url": "/static/media/food-drink-stock-photo.a25b2180.jpg"
+  },
+  {
+    "revision": "73f7f699ffb9ca72b077c320e4717b9e",
+    "url": "/static/media/health-wellness-stock-photo.73f7f699.jpg"
+  },
+  {
+    "revision": "b097f822f6a549d5b84c8ecbddf40564",
+    "url": "/static/media/pets-stock-photo.b097f822.jpg"
+  },
+  {
+    "revision": "0841d84d61242f100fda804bb237a0b9",
+    "url": "/static/media/sports-fitness-stock-photo.0841d84d.jpg"
+  },
+  {
+    "revision": "76d8d06cc8fd1f96333a98741756fdf0",
+    "url": "/static/media/tech-stock-photo.76d8d06c.jpg"
+  },
+  {
+    "revision": "03bcc57789b20d46e119002e33bf081c",
+    "url": "/static/media/socialLogo_vectorBlack.03bcc577.svg"
+  },
+  {
+    "revision": "8e8369c5fa894c1ce3bd21c63a660b82",
+    "url": "/static/media/axe-throwing-stock-image.8e8369c5.jpg"
+  },
+  {
+    "revision": "f63e94ded95d6a357bac4be387e12bbd",
+    "url": "/index.html"
+  }
+];
